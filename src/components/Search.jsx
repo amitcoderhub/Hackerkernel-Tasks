220f581abd01f8e79b2ec3c83193929b9e8907d6
amitcoderhub <amitchauhan6599@gmail.com>
2025-05-07 17:46:32 +0530

    f1

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,13 @@
-const SearchBar = ({ setQuery }) => (
-    <input placeholder="Search..." onChange={e => setQuery(e.target.value)} />
+import { useState } from 'react';
+import SearchBar from './SearchBar';
+
+function App() {
+  const [query, setQuery] = useState('');
+
+  return (
+    <div>
+      <SearchBar setQuery={setQuery} />
+      <p>Search Query: {query}</p>
+    </div>
   );
-  
-  export default SearchBar;
-  
+}
